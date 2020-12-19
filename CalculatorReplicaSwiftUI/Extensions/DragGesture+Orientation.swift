c50b936fe123d0ea1b97273cde32314504fd5f98
DeluxeAlonso <alonso.alvarez@pucp.pe>
2020-12-19 17:04:33 -0500

    Adds comments for calculator operation handler protocol methods

--- CalculatorReplicaSwiftUI/Extensions/DragGesture+Orientation.swift
+++ CalculatorReplicaSwiftUI/Extensions/DragGesture+Orientation.swift
@@ -9,6 +9,13 @@
 import SwiftUI
 
 extension DragGesture.Value {
+    /**
+     Indicates if the drag was a horizontal one.
+
+     - Parameter toleranceOffset: Tolerance value to be used if the user drags in diagonal direction.
+
+     - Returns: True is the drag gesture was in a horizontal way and false if not.
+     */
     func isHorizontalDrag(toleranceOffset: CGFloat = 0.0) -> Bool {
         guard abs(startLocation.y - location.y) <= toleranceOffset else { return false }
         return startLocation.x > location.x || startLocation.x < location.x
