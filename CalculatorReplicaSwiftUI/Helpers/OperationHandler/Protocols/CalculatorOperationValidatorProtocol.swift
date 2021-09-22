ce1c50d907a6ad92af90abf76c5f84e1ebe098bc
DeluxeAlonso <alonso.alvarez.dev@gmail.com>
2021-09-21 23:55:15 -0500

    Updates CalculatorOperationValidatorProtocol methods

--- CalculatorReplicaSwiftUI/Helpers/OperationHandler/Protocols/CalculatorOperationValidatorProtocol.swift
+++ CalculatorReplicaSwiftUI/Helpers/OperationHandler/Protocols/CalculatorOperationValidatorProtocol.swift
@@ -10,6 +10,24 @@ import Foundation
 
 protocol CalculatorOperationValidatorProtocol {
     var calculatorDisplayMaxLimit: Int { get }
+
+    /**
+     * Only plain numbers can be repeated on our calculator result display. Any other option should only appear once.
+     */
+    func shouldProcessCalculatorOption(_ calculatorOption: CalculatorOptionProtocol,
+                                       in calculatorDisplay: String) -> Bool
+
+    /**
+     * We only validate the digits limit if the user is currently entering non-operation options (numbers or decimal).
+     */
+    func areDisplayCharactersInRange(for calculatorDisplay: String,
+                                     and isEnteringNumbers: Bool) -> Bool
+
+    /**
+     * Detects if the user is entering more than one zero on the left side of the number.
+     */
+    func isEnteringSignificantNumber(_ calculatorOption: CalculatorOptionProtocol,
+                                     in calculatorDisplay: String) -> Bool
 }
 
 extension CalculatorOperationValidatorProtocol {
