787ec3d7af891cf2d71feac1077ca9bcfe5c61ce
DeluxeAlonso <alonso.alvarez.dev@gmail.com>
2022-01-23 20:38:51 -0500

    Removes unnecessary imports in CalculatorConfigurationProtocol

--- CalculatorReplicaSwiftUI/Helpers/OperationHandler/Protocols/CalculatorDisplayTrimmerProtocol.swift
+++ CalculatorReplicaSwiftUI/Helpers/OperationHandler/Protocols/CalculatorDisplayTrimmerProtocol.swift
@@ -6,8 +6,6 @@
 //  Copyright © 2020 Alonso. All rights reserved.
 //
 
-import Foundation
-
 protocol CalculatorDisplayTrimmerProtocol {
     func getTrimmedCalculatorDisplay(_ calculatorDisplay: String) -> String
 }
