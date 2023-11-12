bc65abde07b32f30cdca64cef13017475b450849
DeluxeAlonso <alonso.alvarez.dev@gmail.com>
2023-11-12 18:50:53 -0500

    Adds snapshot tests for CalculatorTextButton

--- CalculatorReplicaSwiftUISnapshotTests/CalculatorTextButtonTests.swift
+++ CalculatorReplicaSwiftUISnapshotTests/CalculatorTextButtonTests.swift
@@ -6,31 +6,28 @@
 //  Copyright © 2023 Alonso. All rights reserved.
 //
 
+import SnapshotTesting
+@testable import Calculator
 import XCTest
 
 final class CalculatorTextButtonTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testCalculatorTextButton() {
+        let calculatorButton = CalculatorButtonProtocolMock()
+        calculatorButton.title = "1"
+        calculatorButton.gridSpace = 1
+
+        let view = CalculatorTextButton(button: calculatorButton)
+        assertSnapshot(of: view, as: .image)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    func testCalculatorTextButtonExpandedGridSpace() {
+        let calculatorButton = CalculatorButtonProtocolMock()
+        calculatorButton.title = "1"
+        calculatorButton.gridSpace = 2
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        let view = CalculatorTextButton(button: calculatorButton)
+        assertSnapshot(of: view, as: .image)
     }
 
 }
