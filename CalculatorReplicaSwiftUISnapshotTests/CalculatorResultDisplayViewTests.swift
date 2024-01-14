a64b938fcc7d0ccb5be63a99b4f651d4981a3645
DeluxeAlonso <alonso.alvarez.dev@gmail.com>
2024-01-13 19:21:29 -0500

    Implements CalculatorResultDisplayViewTests

--- CalculatorReplicaSwiftUISnapshotTests/CalculatorResultDisplayViewTests.swift
+++ CalculatorReplicaSwiftUISnapshotTests/CalculatorResultDisplayViewTests.swift
@@ -6,31 +6,22 @@
 //  Copyright © 2024 Alonso. All rights reserved.
 //
 
+@testable import Calculator
+import SnapshotTesting
+import SwiftUI
 import XCTest
 
 final class CalculatorResultDisplayViewTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    func testResultDisplayView() {
+        let calculatorEnvironmentObject = DependencyInjectionFactory.createCalculatorEnvironmentObject()
+        calculatorEnvironmentObject.formattedCalculatorDisplay = "1"
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        let view = CalculatorResultDisplayView()
+            .environmentObject(calculatorEnvironmentObject)
+            .background(Color.black)
+            .previewLayout(.fixed(width: 200, height: 80))
+        assertSnapshot(of: view, as: .image, record: true)
     }
 
 }
