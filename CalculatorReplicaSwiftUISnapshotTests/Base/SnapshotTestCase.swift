e9e781fcad6cf5b6f12f7cdfbd54aa5fcff06bf3
DeluxeAlonso <alonso.alvarez.dev@gmail.com>
2024-01-13 19:34:28 -0500

    Creates snapshots tests case class

---
+++ CalculatorReplicaSwiftUISnapshotTests/Base/SnapshotTestCase.swift
@@ -0,0 +1,9 @@
+//
+//  SnapshotTestCase.swift
+//  CalculatorReplicaSwiftUISnapshotTests
+//
+//  Created by Alonso on 13/01/24.
+//  Copyright © 2024 Alonso. All rights reserved.
+//
+
+import Foundation
