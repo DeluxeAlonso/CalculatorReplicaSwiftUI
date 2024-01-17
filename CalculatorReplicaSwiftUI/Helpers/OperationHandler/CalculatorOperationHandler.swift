504de6fdd633a0cfe346caa2e5a704cbbad1b127
DeluxeAlonso <alonso.alvarez.dev@gmail.com>
2024-01-17 00:59:50 -0500

    Fixes logic for pending binary operation

--- CalculatorReplicaSwiftUI/Helpers/OperationHandler/CalculatorOperationHandler.swift
+++ CalculatorReplicaSwiftUI/Helpers/OperationHandler/CalculatorOperationHandler.swift
@@ -87,7 +87,7 @@ final class CalculatorOperationHadler: CalculatorOperationHandlerProtocol {
             // We leverage operation priority to perform an equal operation automatically before an operation is performed.
             // Sum and minus have a priority of 1. In this scenario if we enter a one of these two three times, an equal
             // operation will be performed before executing the last operation.
-            if let pendingBinaryOperation, pendingBinaryOperation.operationPriority <= operation.priority {
+            if let pendingBinaryOperation, pendingBinaryOperation.operationPriority >= operation.priority {
                 performOperation(.equals)
                 resultValueUpdated = storedCalculatorDisplay.toDouble()
             }
