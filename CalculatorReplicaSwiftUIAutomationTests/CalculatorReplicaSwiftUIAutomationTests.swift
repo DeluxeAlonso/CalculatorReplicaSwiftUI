0ad50d681e958740ca28dc36431e7e963fb20e34
DeluxeAlonso <alonso.alvarez.dev@gmail.com>
2024-01-19 01:57:49 -0500

    Adds simple sum automation test

--- CalculatorReplicaSwiftUIAutomationTests/CalculatorReplicaSwiftUIAutomationTests.swift
+++ CalculatorReplicaSwiftUIAutomationTests/CalculatorReplicaSwiftUIAutomationTests.swift
@@ -10,19 +10,6 @@ import XCTest
 
 final class CalculatorReplicaSwiftUIAutomationTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testSum() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
@@ -33,8 +20,9 @@ final class CalculatorReplicaSwiftUIAutomationTests: XCTestCase {
         app.buttons["five"].tap()
         app.buttons["equal"].tap()
 
-        let resultDisplayText = app.staticTexts["result_display_view_text"]
+        let resultDisplayText = app.staticTexts["result_display_view_text"].firstMatch
         XCTAssertNotNil(resultDisplayText)
-        XCTAssertEqual(resultDisplayText.description, "14")
+        XCTAssertEqual(resultDisplayText.label, "14")
     }
+
 }
