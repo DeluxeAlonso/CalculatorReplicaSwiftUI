0a4df65299cb22698fa21ed80eef5578e9033cb5
DeluxeAlonso <alonso.alvarez.dev@gmail.com>
2024-02-03 23:43:44 -0500

    Creates XCUIElementQuery+CalculatorButtonRepresentable file

---
+++ CalculatorReplicaSwiftUIAutomationTests/XCUIElementQuery+CalculatorButtonRepresentable.swift
@@ -0,0 +1,9 @@
+//
+//  XCUIElementQuery+CalculatorButtonRepresentable.swift
+//  CalculatorReplicaSwiftUIAutomationTests
+//
+//  Created by Alonso on 3/02/24.
+//  Copyright © 2024 Alonso. All rights reserved.
+//
+
+import Foundation
